17e19438383fb96170dfd4a23fa032ffb3faa05b
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-09-18 11:40:39 +0530

    feat: implemented login page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
+import {Poppins} from 'next/font/google'
+import { Button } from "@/components/ui/button";
+import { cn } from '@/lib/utils';
+
+const font = Poppins({
+  subsets:['latin'],
+  weight: ['600']
+})
 
 export default function Home() {
   return (
-    <p className="text-green-600">Hello Auth</p>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-100 to-blue-100">
+      <div className="space-y-6 text-center">
+        <h1 className={cn("text-6xl font-semibold text-slate-700 drop-shadow-md",font.className,)}>Auth</h1>
+        <p className="text-slate-600 text-lg">A simple authentication service</p>
+        <div className="div">
+          <Button variant ='secondary' size='lg' className='font-lg text-slate-100 bg-slate-800'>
+            Sign in
+          </Button>
+        </div>
+      </div>
+    </main>
   );
 }
