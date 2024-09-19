eab016f24d3d810c5226dbb15e2696867475db7c
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-09-19 20:48:15 +0530

    feat: implemented create user in database, created utils for get current user email and getId

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 'use server';
 import { RegisterSchema } from '@/schemas';
-import*as z from 'zod'
+import { db } from '@/lib/db';
+import*as z from 'zod';
+import bcrypt from 'bcrypt'
+import { getUserByEmail } from '@/data/user';
 
 export const register = async(values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -8,5 +11,23 @@ export const register = async(values: z.infer<typeof RegisterSchema>) => {
     if (!validatedFields.success) {
         return{error:'Invalid fields!'};
     }
-        return{success: 'Email sent!'};
+
+    const{ email, name, password} = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return{error: "Email already in use!"};
+    }
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password:hashedPassword,
+        },
+    });
+
+        return{success: 'User created!'};
 }
