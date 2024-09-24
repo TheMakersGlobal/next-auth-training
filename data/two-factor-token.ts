6a88ba9068c2f0b894f9cec7e69b5812c1194384
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-09-24 20:15:03 +0530

    feat: mplemented twoFactor verification mail sending functionality

---
+++ data/two-factor-token.ts
@@ -0,0 +1,24 @@
+import { db } from "@/lib/db";
+
+export const getTwoFactorTokenByToken = async (token: string) => {
+    try {
+        const twoFactortoken = await db.twoFactorToken.findUnique({
+            where: {token}
+        });
+
+        return twoFactortoken;
+    } catch {
+        return null;
+    }
+};
+export const getTwoFactorTokenByEmail = async (email: string) => {
+    try {
+        const twoFactortoken = await db.twoFactorToken.findUnique({
+            where: {email}
+        });
+
+        return twoFactortoken;
+    } catch {
+        return null;
+    }
+};
--- lib/mail.ts
+++ lib/mail.ts
@@ -29,3 +29,14 @@ export const sendPasswordResetEmail = async(
         html: `<p>Click <a href="${resetlink}">here</a>reset password.</p>`
     })
 }
+export const sendTwoFactorTokenEmail = async(
+    email: string,
+    token: string
+) => {
+    await resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: email,
+        subject: '2FA Code',
+        html: `<p>Your 2FA code is: ${token}</p>`
+    })
+}
