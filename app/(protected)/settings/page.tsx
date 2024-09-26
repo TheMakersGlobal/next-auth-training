332194035318742a39f099665d1e82bfa2fb9435
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-09-26 15:20:09 +0530

    feat: implemented update user settings part one

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,30 +1,51 @@
 'use client'
 
-import { useTransition } from "react";
+import * as z from 'zod';
+import {useForm} from 'react-hook-form';
+import { useTransition, useState } from "react";
+import { SettingsShema } from '@/schemas';
 import { logout } from "@/actions/logout";
 import { useCurrentUser } from "@/hooks/use-current-user";
 import { Button } from "@/components/ui/button";
 import { settings } from "@/actions/settings";
 import { useSession } from "next-auth/react";
+import { Form, FormField, FormLabel, FormControl, FormItem, FormDescription, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 
 import { Card,
     CardHeader,
     CardContent,
  } from "@/components/ui/card";
+import { zodResolver } from '@hookform/resolvers/zod';
 
 
 const SettingsPage = () => {
+    const user = useCurrentUser();
+    const [error, setError] = useState<string | undefined>();
+    const [success, setSuccess] = useState<string | undefined>();
     const{update} = useSession()
-    const[isPending, startTransition] = useTransition()
+    const[isPending, startTransition] = useTransition();
+
+    const form = useForm<z.infer<typeof SettingsShema>>({
+        resolver: zodResolver(SettingsShema),
+        defaultValues: {
+            name: user?.name || undefined,
+        }
+    });
     
-    const onclick = () => {
+    const onclick = (values: z.infer<typeof SettingsShema>) => {
         startTransition(() => {
-        settings({
-            name: "New Name!"
+        settings(values)
+        .then((data)=>{
+            if (data.error) {
+                setError(data.error)
+            }
+            if (data.success) {
+                update();
+                setSuccess(data.success)
+            }
         })
-        .then(()=>{
-            update();
-        })
+        .catch(() => setError("something went wrong!"))
     })
     }
     return ( 
@@ -35,9 +56,35 @@ const SettingsPage = () => {
                 </p>
             </CardHeader>
             <CardContent>
-                <Button disabled={isPending} onClick={onclick}>
-                    Update name
-                </Button>
+                <Form {...form}>
+                    <form className='space-y-6'
+                    onSubmit={form.handleSubmit(onclick)}
+                    >
+                        <div className='space-y-4'>
+                            <FormField
+                            control={form.control}
+                            name='name'
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        <FormControl>
+                                            <Input
+                                            {...field}
+                                            placeholder='Jhon Doe'
+                                            disabled={isPending}
+                                            />
+                                        </FormControl>
+                                    </FormLabel>
+                                </FormItem>
+                            )}
+                            >
+                            </FormField>
+                            <Button type='submit'>
+                                Save
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
             </CardContent>
         </Card>
      );
--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import authConfig from "@/auth.config"
 import { getUserById } from "@/data/user"
 import {UserRole} from '@prisma/client';
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation"
+import { getAccountByUserId } from "./data/account"
 
 declare module '@auth/core';
 
@@ -58,11 +59,14 @@ export const {
             }
             if (token.role && session.user) {
                 session.user.role = token.role as UserRole;
-            }            
+            } 
             if (session.user) {
                 session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+            }           
+            if (session.user) {
                 session.user.name = token.name;
-                session.user.email = token.email;
+                session.user.email = token.email as string;
+                session.user.isOAuth = token.isOAuth as boolean;
             }            
             return session;
         },
@@ -72,6 +76,11 @@ export const {
             const existingUser = await getUserById(token.sub);
             if (!existingUser) return token;
 
+            const existingAccount = await getAccountByUserId(
+                existingUser.id
+            )
+
+            token.isOAth = !!existingAccount;
             token.name = existingUser.name;
             token.email = existingUser.email;
             token.role = existingUser.role;
